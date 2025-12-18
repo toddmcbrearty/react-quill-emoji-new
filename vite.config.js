beb19305cd03ee8f29b67c4f1e877c5a6e8fec2c
Todd <todd.mcbrearty@gmail.com>
2025-12-18 12:47:55 -0500

    Enhance GitHub Actions workflow with pnpm setup and improved step naming

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+export default {
+    build: {
+        target: 'es2022',
+        outDir: 'dist',
+        lib: {
+            entry: 'src/quill-emoji.js',
+            name: 'react-quill-emoji-new',
+        }
+    }
+}
